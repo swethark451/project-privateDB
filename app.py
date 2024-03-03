abffc8bf0251fba5249d19f63901fbbc633fd132
swethark451 <148930444+swethark451@users.noreply.github.com>
2024-03-03 17:55:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configure MySQL from environment variables
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'mysql')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', '172.31.69.137')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
